36e9b05847a817bc1b33deb335ab70c091d9d7f6
FedericoCapitani <federico.capitani@outlook.it>
2022-04-22 12:42:47 +0200

    creare finestra input e lista

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,24 @@
 const app = new Vue({
     el: '#app',
-    data: {},
+    data: {
+        tasks: [
+            {
+                text: 'Preparare il pranzo',
+                done: true
+            },
+            {
+                text: 'Mettere a posto il garage',
+                done: false
+            },
+            {
+                text: 'Finire l\'esercizio di Boolean',
+                done: false
+            },
+            {
+                text: 'Studiare la documentazione Vuejs',
+                done: false
+            }
+        ]
+    },
     methods: {}
 })
